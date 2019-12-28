4346183cd09b2a726ee0e9e6049ac0ce66e3912b
racoon63 <racoon63@gmx.net>
2019-12-28 15:27:41 +0100

    add conf base vars and funcs

--- bandwidth-monitor/config.py
+++ bandwidth-monitor/config.py
@@ -3,8 +3,35 @@
 __author__ = 'racoon <racoon63@gmx.net>'
 
 import logging
+import os
 
 class Config(object):
 
-    def __init__(self):
-        pass
+    def __init__(self, config_path):
+        
+        self.speedtest-server = None
+        self.datapath   = "../data/data.json"
+        self.database   = tinydb
+        self.interval   = 60
+        
+        self.dbuser     = None
+        self.dbpassword = None
+        self.dbhost     = None
+        
+        self.loglevel   = info
+        self.logpath    = "../log/bwm.log"
+
+        self.config_path = config_path
+
+        self.environment()
+        self.configFile()
+
+    def environment(self):
+        logging.info("Reading environment variables")
+
+        if 
+
+        return
+
+    def configFile(self):
+        return
