a05cbb3681adfa8945cf765ebf94f0469a0b58cc
racoon63 <racoon63@gmx.net>
2019-12-29 13:08:07 +0100

    rearranged libs and if statements

--- bandwidth-monitor/config.py
+++ bandwidth-monitor/config.py
@@ -13,24 +13,24 @@ class Config(object):
     def __init__(self):
         
         self.speedtest_server = None
-        self.datapath   = "../data/data.json"
-        self.database   = tinydb
         self.interval   = 60
         
-        self.loglevel   = info
-        self.logpath    = "../log/bwm.log"
-        
+        self.database   = tinydb
+        self.datapath   = "../data/data.json"
         self.dbhost     = None
         self.dbuser     = None
         self.dbpassword = None
         
+        self.loglevel   = info
+        self.logpath    = "../log/bwm.log"
+
         if "CONFIG_PATH" in os.environ:
                 logging.debug("Environment variable 'CONFIG_PATH' is available.")
                 logging.debug("Environment variable 'CONFIG_PATH' is: " + self.config_path)
                 self.config_path = os.environ["CONFIG_PATH"]
-            else:
-                logging.debug("Environment variable 'CONFIG_PATH' is not available. Fallback to default value: " + self.config_path)
-                self.config_path = os.path.dirname(os.path.abspath(__file__)) + "../config.ini"
+        else:
+            logging.debug("Environment variable 'CONFIG_PATH' is not available. Fallback to default value: " + self.config_path)
+            self.config_path = os.path.dirname(os.path.abspath(__file__)) + "../config.ini"
         
         self.config_exists = self.checkConfigFile()
 
@@ -38,7 +38,7 @@ class Config(object):
             self.config = self.readConfigFile()
         else:
             self.create()
-            if self.config_exists = self.check():
+            if self.config_exists:
                 self.config = self.read()
             else:
                 logging.fatal("Could not create and read config file")
@@ -102,48 +102,6 @@ class Config(object):
             else:
                 logging.debug("Environment variable 'SPEEDTEST_SERVER' is not set. Fallback to default value: " + self.speedtest_server)
 
-            if "DATABASE" in os.environ:
-                logging.debug("Environment variable 'DATABASE' is available.")
-                logging.debug("Environment variable 'DATABASE' is: " + self.database)
-                self.database = os.environ["DATABASE"]
-            else:
-                logging.debug("Environment variable 'DATABASE' is not available. Fallback to default value: " + self.database)
-
-            if self.database == "tinydb":
-                if "DATAPATH" in os.environ:
-                    logging.debug("Environment variable 'DATAPATH' is available.")
-                    logging.debug("Environment variable 'DATAPATH' is: " + self.datapath)
-                    self.datapath = os.environ["DATAPATH"]
-                else:
-                    logging.debug("Environment variable 'DATAPATH' is not available. Fallback to default value: " + self.datapath)
-            
-            if self.database == "mongodb":
-                if "DBHOST" in os.environ:
-                    logging.debug("Environment variable 'DBHOST' is available.")
-                    logging.debug("Environment variable 'DBHOST' is: " + self.dbhost)
-                    self.dbhost = os.environ["DBHOST"]
-                else:
-                    logging.fatal("Environment variable 'DBHOST' is not available. Can not proceed.")
-                    sys.exit("Exiting")
-                
-                if "DBUSER" in os.environ:
-                    logging.debug("Environment variable 'DBUSER' is available.")
-                    logging.debug("Environment variable 'DBUSER' is: " + self.dbuser)
-                    self.dbuser = os.environ["DBUSER"]
-                else:
-                    logging.fatal("Environment variable 'DBUSER' is not available. Can not proceed.")
-                    sys.exit("Exiting")
-            
-                if "DBPASSWORD" in os.environ:
-                    logging.debug("Environment variable 'DBPASSWORD' is available.")
-                    logging.debug("Environment variable 'DBPASSWORD' is: Haha don't tell you here!")
-                    self.dbpassword = os.environ["DBPASSWORD"]
-                else:
-                    logging.fatal("Environment variable 'DBPASSWORD' is not available. Can not proceed.")
-                    sys.exit("Exiting")
-
-            
-            
             if "INTERVAL" in os.environ:
                 logging.debug("Environment variable 'INTERVAL' is available.")
                 logging.debug("Environment variable 'INTERVAL' is: " + self.interval)
@@ -152,32 +110,76 @@ class Config(object):
                 logging.debug("Environment variable 'INTERVAL' is not available. Fallback to default value: " + self.interval)
 
             config['General'] = {'speedtest-server': self.speedtest_server,
-                                 'datapath': self.database,
-                                 'database': self.datapath,
                                  'interval': self.interval}
+
+            config['Database'] = {}
+
+            if "DATABASE" in os.environ:
+                logging.debug("Environment variable 'DATABASE' is available.")
+                logging.debug("Environment variable 'DATABASE' is: " + self.database)
+                self.database = os.environ["DATABASE"]
+                config['Database']['database'] = self.database
+            else:
+                logging.debug("Environment variable 'DATABASE' is not available. Fallback to default value: " + self.database)
+                config['Database']['database'] = self.database
+
+            if self.database == "tinydb":
+                if "DATAPATH" in os.environ:
+                    logging.debug("Environment variable 'DATAPATH' is available.")
+                    logging.debug("Environment variable 'DATAPATH' is: " + self.datapath)
+                    self.datapath = os.environ["DATAPATH"]
+                    config['Database']['datapath'] = self.datapath
+                else:
+                    logging.debug("Environment variable 'DATAPATH' is not available. Fallback to default value: " + self.datapath)
+                    config['Database']['datapath'] = self.datapath
+            
+            if self.database == "mongodb":
+                if "DBHOST" in os.environ:
+                    logging.debug("Environment variable 'DBHOST' is available.")
+                    logging.debug("Environment variable 'DBHOST' is: " + self.dbhost)
+                    self.dbhost = os.environ["DBHOST"]
+                    config['Database']['dbhost'] = self.dbhost
+                else:
+                    logging.fatal("Environment variable 'DBHOST' is not available. Can not proceed.")
+                    sys.exit("Exiting")
+                
+                if "DBUSER" in os.environ:
+                    logging.debug("Environment variable 'DBUSER' is available.")
+                    logging.debug("Environment variable 'DBUSER' is: " + self.dbuser)
+                    self.dbuser = os.environ["DBUSER"]
+                    config['Database']['dbuser'] = self.dbuser
+                else:
+                    logging.fatal("Environment variable 'DBUSER' is not available. Can not proceed.")
+                    sys.exit("Exiting")
+            
+                if "DBPASSWORD" in os.environ:
+                    logging.debug("Environment variable 'DBPASSWORD' is available.")
+                    logging.debug("Environment variable 'DBPASSWORD' is: Haha don't tell you here!")
+                    self.dbpassword = os.environ["DBPASSWORD"]
+                    config['Database']['dbpassword'] = self.dbpassword
+                else:
+                    logging.fatal("Environment variable 'DBPASSWORD' is not available. Can not proceed.")
+                    sys.exit("Exiting")
+            
+            config['Logging'] = {}
 
             if "LOGLEVEL" in os.environ:
                 logging.debug("Environment variable 'LOGLEVEL' is available.")
                 logging.debug("Environment variable 'LOGLEVEL' is: " + self.loglevel)
                 self.loglevel = os.environ["LOGLEVEL"]
+                config['Logging']['loglevel'] = self.loglevel
             else:
                 logging.debug("Environment variable 'LOGLEVEL' is not available. Fallback to default value: " + self.loglevel)
+                config['Logging']['loglevel'] = self.loglevel
 
             if "LOGPATH" in os.environ:
                 logging.debug("Environment variable 'LOGPATH' is available.")
                 logging.debug("Environment variable 'LOGPATH' is: " + self.logpath)
                 self.logpath = os.environ["LOGPATH"]
+                config['Logging']['logpath'] = self.logpath
             else:
                 logging.debug("Environment variable 'LOGPATH' is not available. Fallback to default value: " + self.logpath)
-
-            
-
-            config['Database'] = {'host': self.dbhost,
-                                 'user': self.dbuser,
-                                 'password': self.dbpassword}
-
-            config['Logging'] = {'loglevel': self.loglevel,
-                                 'logpath': self.logpath}
+                config['Logging']['logpath'] = self.logpath
 
         except Exception:
             logging.fatal("Something went horribly wrong ¯\_(ツ)_/¯")
@@ -189,7 +191,8 @@ class Config(object):
 
     @speedtestServer.setter
     def speedtestServer(self):
-
+        pass
+    
     def validate(self):
 
         logging.debug("Finished reading config file")
