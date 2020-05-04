cc1b3b927e70159f21edecff54c48f0c88ee9ea8
racoon63 <racoon63@gmx.net>
2020-05-04 20:09:16 +0200

    added getter and setter + restructured init

--- app/config.py
+++ app/config.py
@@ -6,11 +6,14 @@ import configparser
 
 class Config:
     """Config class stores config values for the bwm service."""
-    def __init__(self):
-        self.config_file_path = "config.ini"
-        self.config_file = configparser.ConfigParser(
-            empty_lines_in_values=False)
+    def __init__(self, config_file_path="config.ini"):
+        self.config_file_path = config_file_path
 
+        self.defaults()
+        self.get_file_conf()
+        self.get_env_conf()
+
+    def set_defaults(self):
         self.speedtest_server = None
         self.interval = 60
         self.dbtype = "tinydb"
@@ -39,20 +42,94 @@ class Config:
     def get_file_conf(self):
         """ Sets config values from the config.ini file. """
         try:
-            self.config_file.read(self.config_file_path)
+            config_file = configparser.ConfigParser(
+                empty_lines_in_values=False)
+            config_file.read(self.config_file_path)
 
-            self.speedtest_server = self.config_file["General"]["speedtest-server"]
-            self.interval = self.config_file["General"]["interval"]
-            self.dbtype = self.config_file["Database"]["type"]
-            self.datapath = self.config_file["Database"]["datapath"]
-            self.dbhost = self.config_file["Database"]["dbhost"]
-            self.dbuser = self.config_file["Database"]["dbuser"]
-            self.dbpassword = self.config_file["Database"]["dbpassword"]
-            self.loglevel = self.config_file["log"]["loglevel"]
-            self.logpath = self.config_file["log"]["logpath"]
+            self.speedtest_server = config_file["General"]["speedtest-server"]
+            self.interval = config_file["General"]["interval"]
+            self.dbtype = config_file["Database"]["type"]
+            self.datapath = config_file["Database"]["datapath"]
+            self.dbhost = config_file["Database"]["dbhost"]
+            self.dbuser = config_file["Database"]["dbuser"]
+            self.dbpassword = config_file["Database"]["dbpassword"]
+            self.loglevel = config_file["log"]["loglevel"]
+            self.logpath = config_file["log"]["logpath"]
         except configparser.ParsingError:
             pass
         except configparser.NoSectionError:
             pass
         except configparser.NoOptionError:
             pass
+
+    @property
+    def speedtest_server(self):
+        return self.speedtest_server
+
+    @speedtest_server.setter
+    def speedtest_server(self, value):
+        self.speedtest_server = value
+
+    @property
+    def interval(self):
+        return self.interval
+
+    @interval.setter
+    def interval(self, value):
+        self.interval = value
+    
+    @property
+    def dbtype(self):
+        return self.dbtype
+
+    @dbtype.setter
+    def dbtype(self, value):
+        self.dbtype = value
+
+    @property
+    def datapath(self):
+        return self.datapath
+
+    @datapath.setter
+    def datapath(self, value):
+        self.datapath = value
+
+    @property
+    def dbhost(self):
+        return self.dbhost
+
+    @dbhost.setter
+    def dbhost(self, value):
+        self.dbhost = value
+
+    @property
+    def dbuser(self):
+        return self.dbuser
+
+    @dbuser.setter
+    def dbuser(self, value):
+        self.dbuser = value
+
+    @property
+    def dbpassword(self):
+        return self.dbpassword
+
+    @dbpassword.setter
+    def dbpassword(self, value):
+        self.dbpassword = value
+
+    @property
+    def loglevel(self):
+        return self.loglevel
+
+    @loglevel.setter
+    def loglevel(self, value):
+        self.loglevel = value
+
+    @property
+    def logpath(self):
+        return self.logpath
+
+    @logpath.setter
+    def logpath(self, value):
+        self.logpath = value
