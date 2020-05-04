9c5ac815d636d7fb16278f87589cde019753a4ed
racoon63 <racoon63@gmx.net>
2020-05-04 20:24:31 +0200

    added method doc strings

--- app/config.py
+++ app/config.py
@@ -9,11 +9,12 @@ class Config:
     def __init__(self, config_file_path="config.ini"):
         self.config_file_path = config_file_path
 
-        self.defaults()
+        self.set_defaults()
         self.get_file_conf()
         self.get_env_conf()
 
     def set_defaults(self):
+        """ Sets the default variable values. """
         self.speedtest_server = None
         self.interval = 60
         self.dbtype = "tinydb"
@@ -64,72 +65,93 @@ class Config:
 
     @property
     def speedtest_server(self):
+        """ Returns the speedtest server config variable. """
         return self.speedtest_server
 
     @speedtest_server.setter
     def speedtest_server(self, value):
-        self.speedtest_server = value
+        """ Sets the speedtest server config variable. """
+        if value == "auto" or value > 0 and value < 50000:
+            self.speedtest_server = value
+        else:
+            raise KeyError("Speedtest server is not valid.")
 
     @property
     def interval(self):
+        """ Returns the interval config variable. """
         return self.interval
 
     @interval.setter
     def interval(self, value):
+        """ Sets the interval config variable. """
         self.interval = value
     
     @property
     def dbtype(self):
+        """ Returns the dbtype config variable. """
         return self.dbtype
 
     @dbtype.setter
     def dbtype(self, value):
+        """ Sets the dbtype config variable. """
         self.dbtype = value
 
     @property
     def datapath(self):
+        """ Returns the datapath config variable. """
         return self.datapath
 
     @datapath.setter
     def datapath(self, value):
+        """ Sets the datapath config variable. """
         self.datapath = value
 
     @property
     def dbhost(self):
+        """ Returns the dbhost config variable. """
         return self.dbhost
 
     @dbhost.setter
     def dbhost(self, value):
+        """ Sets the dbhost config variable. """
         self.dbhost = value
 
     @property
     def dbuser(self):
+        """ Returns the dbuser config variable. """
         return self.dbuser
 
     @dbuser.setter
     def dbuser(self, value):
+        """ Sets the dbuser config variable. """
         self.dbuser = value
 
     @property
     def dbpassword(self):
+        """ Returns the dbpassword config variable. """
         return self.dbpassword
 
     @dbpassword.setter
     def dbpassword(self, value):
+        """ Sets the dbpassword config variable. """
         self.dbpassword = value
 
     @property
     def loglevel(self):
+        """ Returns the loglevel config variable. """
         return self.loglevel
 
     @loglevel.setter
     def loglevel(self, value):
+        """ Sets the loglevel config variable. """
         self.loglevel = value
 
     @property
     def logpath(self):
+        """ Returns the logpath config variable. """
         return self.logpath
 
     @logpath.setter
     def logpath(self, value):
+        """ Sets the logpath config variable. """
         self.logpath = value
