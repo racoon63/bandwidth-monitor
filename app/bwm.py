3aea594e09f6e4aa54836f4dd0c73beffd625946
racoon63 <racoon63@gmx.net>
2020-05-05 22:48:51 +0200

    added storage handler

--- app/bwm.py
+++ app/bwm.py
@@ -4,14 +4,19 @@ import time
 
 from config import Config
 from measurement import Measurement
-from storage import Storage
+from storage import StorageInterface
 
 
 class BandwidthMonitor:
     """ BandwidthMonitor class is the core class of the bwm service. """
     conf = Config()
     msment = Measurement()
-    backend = Storage()
+    storage_handler = StorageInterface(datapath=conf.datapath,
+                                       dbtype=conf.dbtype,
+                                       dbhost=conf.dbhost,
+                                       dbuser=conf.dbuser,
+                                       dbpassword=conf.dbpassword,
+                                       dbname="bwm")
 
     @classmethod
     def run(cls):
@@ -19,6 +24,5 @@ class BandwidthMonitor:
         while True:
             starttime = time.time()
             results = cls.msment.measure()
-            print(results)
-            # self.backend.save(results)
+            cls.storage_handler.store(cls.conf.dbtype, results)
             time.sleep(cls.conf.interval - ((time.time() - starttime) % cls.conf.interval))
